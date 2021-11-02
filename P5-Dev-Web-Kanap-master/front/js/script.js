a41389dbbaef6ba7e105ac1190724584eef6a929
Mahiet Bruno <devwebmb@gmail.com>
2021-11-02 19:06:12 +0100

    Logique de l'affichage page d'accueil

--- P5-Dev-Web-Kanap-master/front/js/script.js
+++ P5-Dev-Web-Kanap-master/front/js/script.js
@@ -1,1 +1,21 @@
 // http://localhost:3000/api/products
+let url = "http://localhost:3000/api/products";
+
+fetch(url)
+  .then(function (res) {
+    // console.log(res); //test1 plan d'acceptation
+    return res.json();
+  })
+  .catch((err) => console.log("erreur :" + err))
+  .then(function (data) {
+    for (let element of data) {
+      // console.log(data);
+      document.querySelector("#items").innerHTML += ` <a href="">
+      <article>
+        <img src="${element.imageUrl}" alt="${element.altTxt}">
+        <h3 class="productName">${element.name}</h3>
+        <p class="productDescription">${element.description}</p>
+      </article>
+    </a>`;
+    }
+  });
