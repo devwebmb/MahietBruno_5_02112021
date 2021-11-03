b374d30fac1059b52a53c53004edc1f4bd9b92ce
Mahiet Bruno <devwebmb@gmail.com>
2021-11-03 17:47:15 +0100

    Début de la page product

--- P5-Dev-Web-Kanap-master/front/js/product.js
+++ P5-Dev-Web-Kanap-master/front/js/product.js
@@ -1,1 +1,30 @@
 // http://localhost:3000/api/products
+let api = "http://localhost:3000/api/products";
+let urlData = new URLSearchParams(window.location.search);
+let urlId = urlData.get("id");
+
+//Récupération des données, de l'url, ajout dans le code html de la page product de façon dynamique
+fetch(api).then((res) =>
+  res.json().then(function (data) {
+    for (let element of data) {
+      console.log(data);
+      if (element._id === urlId) {
+        document.querySelector(
+          "body > main > div > section > article > div.item__img"
+        ).innerHTML = `<img src="${element.imageUrl}" alt="${element.altTxt}"></img>`;
+        document.querySelector("#title").innerHTML = `${element.name}`;
+        document.querySelector("#price").innerHTML = `${element.price}`;
+        document.querySelector(
+          "#description"
+        ).innerHTML = `${element.description}`;
+        let colors = element.colors;
+        for (i = 0; i < colors.length; i++) {
+          console.log(colors[i]);
+          document.querySelector(
+            "#colors"
+          ).innerHTML += `<option value=${colors[i]}>${colors[i]}</option>`;
+        }
+      }
+    }
+  })
+);
--- P5-Dev-Web-Kanap-master/front/js/script.js
+++ P5-Dev-Web-Kanap-master/front/js/script.js
@@ -1,21 +1,61 @@
 // http://localhost:3000/api/products
-let url = "http://localhost:3000/api/products";
+let api = "http://localhost:3000/api/products";
 
-fetch(url)
-  .then(function (res) {
-    // console.log(res); //test1 plan d'acceptation
-    return res.json();
-  })
-  .catch((err) => console.log("erreur :" + err))
-  .then(function (data) {
-    for (let element of data) {
-      // console.log(data);
-      document.querySelector("#items").innerHTML += ` <a href="">
-      <article>
-        <img src="${element.imageUrl}" alt="${element.altTxt}">
-        <h3 class="productName">${element.name}</h3>
-        <p class="productDescription">${element.description}</p>
-      </article>
-    </a>`;
-    }
-  });
+//Récupération des données
+// fetch(api)
+//   .then(function (res) {
+//     console.log(res); //test1 plan d'acceptation
+//     return res.json();
+//   })
+//   .catch((err) => console.log("erreur :" + err))
+//   .then(function (data) {
+//     console.log(data);
+//     for (let element of data) {
+//       //Ajout des différents articles de façon dynamique
+//       document.querySelector(
+//         "#items"
+//       ).innerHTML += ` <a href="./product.html?id=${element._id}">
+//       <article>
+//         <img src="${element.imageUrl}" alt="${element.altTxt}">
+//         <h3 class="productName">${element.name}</h3>
+//         <p class="productDescription">${element.description}</p>
+//       </article>
+//     </a>`;
+//     }
+//   });
+
+// const fetchApi = () => {
+//   return new Promise((resolve, reject) => {
+//     fetch(api)
+//       .then((res) => {
+//         resolve(res);
+//       })
+//       .catch((err) => {
+//         reject(err);
+//       });
+//   });
+// };
+
+// //const uneFonction = () => {
+//   // ... plein de choses mais pas trop
+//   return quelqueChose
+// }
+
+// const uneAutreFonction = (data) => {
+//   // ... plein de choses qui utilisent la data
+// }
+
+// const data = uneFunction()
+// uneAutreFonction(data)
+const fetchApi = () => {
+  fetch(api)
+    .then((res) => {
+      console.log(res);
+      return res.json();
+    })
+    .then((data) => {
+      let array = data;
+      console.log(array);
+      return array;
+    });
+};
