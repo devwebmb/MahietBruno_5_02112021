4ee2773e1d684de449a5010f4bb40101efd59d99
Mahiet Bruno <devwebmb@gmail.com>
2021-11-10 08:24:29 +0100

    Commit avant mentorat

--- P5-Dev-Web-Kanap-master/front/js/product.js
+++ P5-Dev-Web-Kanap-master/front/js/product.js
@@ -5,6 +5,7 @@ let urlData = new URLSearchParams(window.location.search);
 // console.log(urlData);
 let urlId = urlData.get("id");
 // console.log(urlId);
+
 //récupération des données dans l'api
 const fetchApi = () => {
   return fetch(api)
@@ -13,39 +14,10 @@ const fetchApi = () => {
 };
 
 //Insertion des données dans le dom, product.html et dans le panier
-// const displayProduct = () => {
-//   fetchApi().then((data) => {
-//     for (element of data) {
-//       if (element._id === urlId) {
-//         document.querySelector(
-//           "body > main > div > section > article > div.item__img"
-//         ).innerHTML = `<img src="${element.imageUrl}" alt="${element.altTxt}"></img>`;
-//         document.querySelector("#title").innerHTML = `${element.name}`;
-//         document.querySelector("#price").innerHTML = `${element.price}`;
-//         document.querySelector(
-//           "#description"
-//         ).innerHTML = `${element.description}`;
-//         let colors = element.colors;
-//         for (i = 0; i < colors.length; i++) {
-//           document.querySelector(
-//             "#colors"
-//           ).innerHTML += `<option value=${colors[i]}>${colors[i]}</option>`;
-//         }
-//       }
-//     }
-//   });
-// };
-
-// displayProduct();
-let productId;
-
 const displayProduct = () => {
   fetchApi().then((data) => {
-    // console.log(data);
     for (element of data) {
-      // insertion des données dans le dom en fonction du canapé choisi en page d'accueil
       if (element._id === urlId) {
-        productId = element._id;
         document.querySelector(
           "body > main > div > section > article > div.item__img"
         ).innerHTML = `<img src="${element.imageUrl}" alt="${element.altTxt}"></img>`;
@@ -65,29 +37,41 @@ const displayProduct = () => {
   });
 };
 
-//récupérer la couleur
-let colorChoice;
-document.querySelector("#colors").addEventListener("change", (event) => {
-  colorChoice = event.target.value;
-});
-
-//Récupérer la quantité sélectionnée
-let quantityChoice;
-document.querySelector("#quantity").addEventListener("input", (event) => {
-  quantityChoice = event.target.value;
-});
-
-const addToLocalStorage = () => {
-  //Récupération et stockage des données à envoyer au localstorage
-  let addToStorage = {
-    id: productId,
-    color: colorChoice,
-    quantity: quantityChoice,
-  };
-
-  //Création d'une variable pour récupérer le localstrorage
-  let localStorageContent = JSON.parse(localStorage.getItem("product"));
-};
-
 displayProduct();
-addToCart();
+// let productId;
+
+// //récupérer la couleur
+// let colorChoice;
+// document.querySelector("#colors").addEventListener("change", (event) => {
+//   colorChoice = event.target.value;
+// });
+
+// //Récupérer la quantité sélectionnée
+// let quantityChoice;
+// document.querySelector("#quantity").addEventListener("input", (event) => {
+//   quantityChoice = event.target.value;
+// });
+
+// //Récupération et stockage des données à envoyer au localstorage
+// let addToStorage = {
+//   id: productId,
+//   color: colorChoice,
+//   quantity: quantityChoice,
+// };
+
+document.querySelector("#addToCart").addEventListener("click", () => {
+  let color = document.querySelector("#colors").value;
+  console.log(color);
+  if (color == "") {
+    alert("Veuillez choisir une couleur avant de d'ajouter au panier");
+    return;
+  }
+  localStorage.setItem("product", JSON.stringify(addToStorage));
+});
+
+// displayProduct();
+
+//methode find js https://www.digitalocean.com/community/tutorials/js-array-search-methods-fr#find
+// const alligator = ["thick scales", 80, "4 foot tail", "rounded snout"];
+
+// alligator.find(el => el.length < 12); // returns '4 foot tail'
